82a0b52a5d470016ad83c0ae9714568cba4c4828
Kacper Zienkiewicz <k09028449@gmail.com>
2025-05-03 11:51:55 +0200

    Write out postprocessing results to file

--- letrng_postprocess.c
+++ letrng_postprocess.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FILENAME "13million.bin"
+#define FILE_SOURCE "13million.bin"
+#define FILE_DESTINATION "postprocessed.bin"
 
 unsigned long long fold_bits(unsigned long long B) {
 	unsigned long long R = 0; // Initial value of R is not disclosed in the paper
@@ -14,20 +15,35 @@ unsigned long long fold_bits(unsigned long long B) {
 	return R;
 }
 
+int open_files(char* rname, FILE** read,
+		char* wname, FILE** write) {
+	*read = fopen(rname, "rb");
+	if (!*read) {
+		return 1;
+	}
+	*write = fopen(wname, "wb");
+	if (!*write) {
+		fclose(*read);
+		return 1;
+	}
+	return 0;
+}
+
 int main(void) {
 	unsigned long long entropy_pool = 0;	
 	int entropy_count = 0;
+	FILE *src, *dst;
 
-	FILE *file = fopen(FILENAME, "rb");	
-	if (!file) {
-		perror("Failed to open file");
+	int res = open_files(FILE_SOURCE, &src, FILE_DESTINATION, &dst);
+	if (res) {
+		perror("Failed to open files!");
 		return 1;
 	}
 
 	// fair coin
 	unsigned long long x64, y64;
-	while (fread(&x64, sizeof(unsigned long long), 1, file) == 1 &&
-		fread(&y64, sizeof(unsigned long long), 1, file) == 1) {
+	while (fread(&x64, sizeof(unsigned long long), 1, src) == 1 &&
+		fread(&y64, sizeof(unsigned long long), 1, src) == 1) {
 		unsigned long long a = fold_bits(x64);
 		unsigned long long b = fold_bits(y64);
 		if (a == b) {
@@ -40,13 +56,17 @@ int main(void) {
 			entropy_pool |= 0;
 		entropy_count++;
 		if (entropy_count % 64 == 0) {
-			printf("%llu\n", entropy_pool);
-			break;
+			size_t written = fwrite(&entropy_pool, sizeof(unsigned long long), 1, dst);
+			if (1 != written) {
+				perror("Error writing to file!");
+				goto cleanup;
+			}
+			entropy_count = 0;
 		}
 	}
-	if (ferror(file)) {
-		perror("Error reading file");
-	}
-	fclose(file);
+
+cleanup:
+	fclose(dst);
+	fclose(src);
 	return 0;
 }
